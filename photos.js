cfd202d4aede3dd00b130a8339d9b88bad11973f
sefat006 <sefatur76@gmail.com>
2024-01-04 18:08:26 +0600

    edited

--- photos.js
+++ photos.js
@@ -1,22 +1,24 @@
-const loadPhotos = async() => {
+const loadPhotos = async () => {
     const url = `https://jsonplaceholder.typicode.com/photos`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhotos(data.data);
-}
+    displayPhotos(data);
+};
 loadPhotos();
 
 const displayPhotos = (photos) => {
-    const photosContainer = document.getElementById('photos-container');
-    photos.forEach = (photo =>{
-        photoDiv = document.createElement('div');
-        photoDiv.classlist.add('grid grid-cols-3');
+    //console.log(photos);
+    const photosContainer = document.getElementById("photos-container");
+    photosContainer.classList.add("grid");
+    photosContainer.classList.add("grid-cols-3");
+    photos.forEach((photo) => {
+        const photoDiv = document.createElement("div");
         photoDiv.innerHTML = `
             <div class="width-250px shadow-lg border-2">
-            <img src="${photo.albumId}" alt="">
-                <p class="text-xl mb-5">${photo.data.data.title}</p>
+            <img src="${photo.thumbnailUrl}" alt="">
+                <p class="text-xl mb-5">${photo.title}</p>
             </div>
         `;
         photosContainer.appendChild(photoDiv);
-    })
-}
+    });
+};
